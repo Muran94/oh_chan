bb111e85f2458967a398d1e27227b4e149ee02ad
Muran94 <headiv94@gmail.com>
2017-10-17 22:07:18 +0900

    MessageBoardモデルの追加 Commentモデルの追加

--- message_board/models.py
+++ message_board/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class MessageBoard(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+
+class Comment(models.Model):
+    message_board = models.ForeignKey(MessageBoard, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1000)
+    handle_name = models.CharField(max_length=100)
